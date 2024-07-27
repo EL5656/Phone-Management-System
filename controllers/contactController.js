1b75bb0a4d1f8b1bb5ab446c2f58ff443c3d20e5
elaine460 <102460032+elaine460@users.noreply.github.com>
2024-07-27 12:31:36 +0800

    Update Contact

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -31,7 +31,18 @@ const getContactById = asyncHandler(async(req,res)=>{
 })
 
 const updateContact = asyncHandler(async(req,res)=>{
-    res.status(200).json({message: `Update contact for ${req.params.id}`});
+    const contact = await Contact.findById(req.params.id);
+    if (!contact) {
+        res.status(404);
+        throw new Error("Contact not found!");
+    }
+
+    const updatedContact = await Contact.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {new: true}
+    );
+    res.status(200).json(updatedContact);
 })
 
 const deleteContanct = asyncHandler(async(req,res)=>{
